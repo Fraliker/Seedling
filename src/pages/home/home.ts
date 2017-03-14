2735f9bf73947f5f0cafdab2145d4f09303ebe61
Shirlyn Prabahar <pshirlyn@gmail.com>
2017-03-14 02:06:55 -0700

    added list refreshing capabilities

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,10 @@ import { DataStore } from '../../providers/data-store';
 
 import { ItemViewPage } from '../item-view/item-view';
 
+import { ToastController } from 'ionic-angular';
+
+import * as moment from 'moment';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -17,87 +21,22 @@ export class HomePage {
 	public basic;
 	public student;
 
-  constructor(public navCtrl: NavController, public dataServe: DataStore) {
+  constructor(public navCtrl: NavController, public dataServe: DataStore, public toastCtrl: ToastController) {
   	this.dataServe.getData().then((list) => {
   		if(list) {
   			this.basic = JSON.parse(list);
   		}
-
   	});
   }
 
   ionViewDidLoad() {
-  	  this.basic = [{
-    enabled: true,
-    text: "Communicate with at least one known family member/ friend in person (not over phone or through text)"
-  }, {
-    enabled: true,
-    text: "Talk to someone new today"
-  },{
-    enabled: false,
-    text: "Journal Write - Write about some things that you accomplished today or that you are proud of"
-  },{
-    enabled: false,
-    text: "Journal Write - Plan out your day for tomorrow - What things do you want to get done."
-  },{
-    enabled: true,
-    text: "Finish at least one part of a long time project (personal or from work)"
-  },{
-    enabled: true,
-    text: "Complete a relaxation hour - Take time away from work and do something you genuinely enjoy"
-  },{
-    enabled: true,
-    text: "Read a new book about a topic you are interested in"
-  },{
-    enabled: true,
-    text: "Practice discernment while watching TV or reading the news"
-  },{
-    enabled: true,
-    text: "Take a long shower/bath"
-  },{
-    enabled: true,
-    text: "Go jogging for at least 30 minutes"
-  },];
+  
 
-  this.student = [{
-    enabled: true,
-    text: "Communicate with at least one known family member/ friend in person (not over phone or through text)"
-  }, {
-    enabled: true,
-    text: "Talk to someone new today"
-  },{
-    enabled: true,
-    text: "Think about how today went - Write about some things that you accomplished today or that you are proud of"
-  },{
-    enabled: true,
-    text: "Journal Write - Plan out the next week - What tests/ projects/ assignments do you have coming up? Set manageable goals for yourself to accomplish by this time next week."
-  },{
-    enabled: true,
-    text: "Change up your studying tactics - Analyze what has been working for you and what has not. Try developing a new study method in order to maximize those beneftis"
-  },{
-    enabled: true,
-    text: "Go over notes taken in class - "
-  },{
-    enabled: true,
-    text: "Take at least an hour break while studying tonight"
-  },{
-    enabled: true,
-    text: "Read ahead in a class you are struggling in"
-  },{
-    enabled: true,
-    text: "Take a walk around the neighborhood"
-  },{
-    enabled: true,
-    text: "Sleep an hour before you usually do"
-  },];
-
-
-  console.log(this.basic);	
   }
 
-  addItem() {
-
-
+  getTime() {
+    let now = moment().format('LLLL');
+    console.log(now);
   }
   
   viewItem(item) {
@@ -113,19 +52,21 @@ export class HomePage {
   }
 
   complete(item) {
-    item.enabled = false;
-    this.dataServe.save(this.basic);
-
-  }
-
-  delete(item) {
     this.basic.splice(this.basic.indexOf(item), 1);
     this.dataServe.save(this.basic);
+    let toast = this.toastCtrl.create({
+        message: 'Congratulations for completing a task!',
+        duration: 2000
+      });
+    toast.present();
   }
 
   refresh() {
-    this.basic = this.student;
-    this.dataServe.save(this.basic);
+    this.dataServe.getBasic().then((basic) => {
+      if(basic) {
+        this.basic = JSON.parse(basic);
+      }
+    });
   }
 
 }
